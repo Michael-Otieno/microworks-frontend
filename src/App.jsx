c7cba7f7e5331b748b76aeaddd7f5e50bc11002b
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-11-11 18:23:44 +0300

    add and configure react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import Details from "./components/Details"
-import './App.css';
+import Details from "./components/Details";
+import "./App.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
-
   return (
     <div className="font-['Inter']">
-      <Details/>
+      <Routes>
+        <Route path="/home" element={<Home/>} />
+        <Route path="/details" element={<Details />} />
+        <Route path="*" element={<Navigate to="/home" replace />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
