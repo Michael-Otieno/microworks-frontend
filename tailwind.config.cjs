a9cff0c89cf699442601309d1a5177528526cc4f
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-11-11 06:27:07 +0300

    configure colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,9 +2,26 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        '128': '28rem',
+      }
+    },
+    colors: {
+      'green': '#29CD9C',
+      'inputs': '#F3F4F6',
+      'thead':'#E5E5E5',
+      'tableBorderColor':'#CCCCCC',
+      'white':'#FFFFFF',
+      'black':'#000000',
+      'red':'#FE0C0C',
+      'blue':' #327EFF'
+      
+      
+    },
   },
   plugins: [],
 }
